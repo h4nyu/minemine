efdbea6c16c5866bbbbf32f10cf86f732cced49b
xinyuan-yao <yao.ntno@gmail.com>
2021-05-08 23:56:09 +0900

    add 3090

---
+++ 3090.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+GPUID=$1
+GPU_OFFSET=-300
+MEM_OFFSET=1000
+
+nvidia-smi -i $GPUID -pl 285
+DISPLAY=:0 nvidia-settings --verbose \
+    -a "[gpu:$GPUID]/GPUPowerMizerMode=1" \
+    -a "[gpu:$GPUID]/GPUGraphicsClockOffset[4]=$GPU_OFFSET" \
+    -a "[gpu:$GPUID]/GPUMemoryTransferRateOffset[4]=$MEM_OFFSET"
