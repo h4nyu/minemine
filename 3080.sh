5457720a0b180093c1ed3444965214f24bceb72a
xinyuan-yao <yao.ntno@gmail.com>
2021-05-09 00:41:54 +0900

    fix?

--- 3080.sh
+++ 3080.sh
@@ -2,7 +2,7 @@
 
 GPUID=$1
 GPU_OFFSET=-200
-MEM_OFFSET=950
+MEM_OFFSET=1600
 
 nvidia-smi -i $GPUID -pl 230 
 DISPLAY=:0 nvidia-settings --verbose \
--- 3090.sh
+++ 3090.sh
@@ -2,7 +2,7 @@
 
 GPUID=$1
 GPU_OFFSET=-300
-MEM_OFFSET=1000
+MEM_OFFSET=500
 
 nvidia-smi -i $GPUID -pl 285
 DISPLAY=:0 nvidia-settings --verbose \
