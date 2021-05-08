ce818c968924dc7eb8d730207252653c965ed4eb
xinyuan-yao <yao.ntno@gmail.com>
2021-05-08 23:31:01 +0900

    fix

--- 3080.sh
+++ 3080.sh
@@ -2,13 +2,10 @@
 
 GPUID=$1
 GPU_OFFSET=-300
-MEM_OFFSET=1000
+MEM_OFFSET=100
 
 nvidia-smi -i $GPUID -pl 230 
-DISPLAY=:0 nvidia-settings -a "[gpu:$GPUID]/GPUPowerMizerMode=1"
-DISPLAY=:0 nvidia-settings -a "[gpu:$GPUID]/GPUGraphicsClockOffset[2]=$GPU_OFFSET"
-DISPLAY=:0 nvidia-settings -a "[gpu:$GPUID]/GPUGraphicsClockOffset[3]=$GPU_OFFSET"
-DISPLAY=:0 nvidia-settings -a "[gpu:$GPUID]/GPUGraphicsClockOffset[4]=$GPU_OFFSET"
-DISPLAY=:0 nvidia-settings -a "[gpu:$GPUID]/GPUMemoryTransferRateOffset[2]=$MEM_OFFSET"
-DISPLAY=:0 nvidia-settings -a "[gpu:$GPUID]/GPUMemoryTransferRateOffset[3]=$MEM_OFFSET"
-DISPLAY=:0 nvidia-settings -a "[gpu:$GPUID]/GPUMemoryTransferRateOffset[4]=$MEM_OFFSET"
+DISPLAY=:0 nvidia-settings --verbose \
+    -a "[gpu:$GPUID]/GPUPowerMizerMode=1" \
+    -a "[gpu:$GPUID]/GPUGraphicsClockOffset[4]=$GPU_OFFSET" \
+    -a "[gpu:$GPUID]/GPUMemoryTransferRateOffset[4]=$MEM_OFFSET"
