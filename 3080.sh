4128e4882ab193ca12fa881799c83c5d90d36b46
xinyuan-yao <yao.ntno@gmail.com>
2021-05-09 01:04:58 +0900

    fix

--- 3080.sh
+++ 3080.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 GPUID=$1
-GPU_OFFSET=-200
-MEM_OFFSET=1600
+GPU_OFFSET=-300
+MEM_OFFSET=1700
 
 nvidia-smi -i $GPUID -pl 230 
 DISPLAY=:0 nvidia-settings --verbose \
-    -a "[gpu:$GPUID]/GPUPowerMizerMode=1" \
-    -a "[gpu:$GPUID]/GPUGraphicsClockOffset[4]=$GPU_OFFSET" \
-    -a "[gpu:$GPUID]/GPUMemoryTransferRateOffset[4]=$MEM_OFFSET"
+    -a "[gpu:$GPUID]/GPUGraphicsClockOffsetAllPerformanceLevels=$GPU_OFFSET" \
+    -a "[gpu:$GPUID]/GPUMemoryTransferRateOffsetAllPerformanceLevels=$MEM_OFFSET"
--- 3090.sh
+++ 3090.sh
@@ -2,7 +2,7 @@
 
 GPUID=$1
 GPU_OFFSET=-300
-MEM_OFFSET=500
+MEM_OFFSET=1600
 
 nvidia-smi -i $GPUID -pl 285
 DISPLAY=:0 nvidia-settings --verbose \
