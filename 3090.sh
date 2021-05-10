b03e4cd6c32baf21a274606cab0bf926e169cf8a
xinyuan-yao <yao.ntno@gmail.com>
2021-05-11 00:41:53 +0900

    fix 3090

--- 3090.sh
+++ 3090.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 GPUID=$1
-GPU_OFFSET=-300
-MEM_OFFSET=1600
+GPU_OFFSET=-400
+MEM_OFFSET=2000
 
-nvidia-smi -i $GPUID -pl 285
+nvidia-smi -i $GPUID -pl 300
 DISPLAY=:0 nvidia-settings --verbose \
     -a "[gpu:$GPUID]/GPUPowerMizerMode=1" \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffset[4]=$GPU_OFFSET" \
