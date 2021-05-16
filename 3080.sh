b5ef95a959e3b9fde88ac937abc99daa4d637a74
xinyuan-yao <yao.ntno@gmail.com>
2021-05-16 16:13:34 +0900

    3080

--- 3080.sh
+++ 3080.sh
@@ -4,7 +4,7 @@ GPUID=$1
 GPU_OFFSET=-300
 MEM_OFFSET=2000
 
-nvidia-smi -i $GPUID -pl 230 
+nvidia-smi -i $GPUID -pl 225
 DISPLAY=:0 nvidia-settings --verbose \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffsetAllPerformanceLevels=$GPU_OFFSET" \
     -a "[gpu:$GPUID]/GPUMemoryTransferRateOffsetAllPerformanceLevels=$MEM_OFFSET"
