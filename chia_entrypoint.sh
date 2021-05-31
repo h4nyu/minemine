6480f1b3dfe82fdcabac89932c6a130112893dce
h4nyu <yao.ntno@gmail.com>
2021-05-31 15:23:08 +0000

    ?

--- chia_entrypoint.sh
+++ chia_entrypoint.sh
@@ -10,10 +10,11 @@ if [[ ! "$(ls -A /plots)" ]]; then
   echo "Plots directory appears to be empty and you have not specified another, try mounting a plot directory with the docker -v command "
 fi
 
-chia plots add -d /plots
+for d in /mnt/*/ ; do
+    chia plots add -d $d
+done
 
 sed -i 's/localhost/127.0.0.1/g' ~/.chia/mainnet/config/config.yaml
 
 chia start farmer
-
-$@
+while true; do sleep 30; done;
