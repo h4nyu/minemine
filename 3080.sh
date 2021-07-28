c11e55057dc5185e5d4cbb7c2eca2127f5ab35df
yao <yao.ntno@gmail.com>
2021-07-28 11:23:35 +0000

    reduce wat

--- 3080.sh
+++ 3080.sh
@@ -2,9 +2,9 @@
 
 GPUID=$1
 GPU_OFFSET=-300
-MEM_OFFSET=1950
+MEM_OFFSET=1900
 
-nvidia-smi -i $GPUID -pl 225
+nvidia-smi -i $GPUID -pl 220
 DISPLAY=:0 nvidia-settings --verbose \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffsetAllPerformanceLevels=$GPU_OFFSET" \
     -a "[gpu:$GPUID]/GPUMemoryTransferRateOffsetAllPerformanceLevels=$MEM_OFFSET"
