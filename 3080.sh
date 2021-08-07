bb82208018140108de75f15fe5ee2a4285e677ad
yao <yao.ntno@gmail.com>
2021-08-07 06:49:44 +0000

    ajust watt

--- 3080.sh
+++ 3080.sh
@@ -4,7 +4,7 @@ GPUID=$1
 GPU_OFFSET=-300
 MEM_OFFSET=1900
 
-nvidia-smi -i $GPUID -pl 220
+nvidia-smi -i $GPUID -pl 222
 DISPLAY=:0 nvidia-settings --verbose \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffsetAllPerformanceLevels=$GPU_OFFSET" \
     -a "[gpu:$GPUID]/GPUMemoryTransferRateOffsetAllPerformanceLevels=$MEM_OFFSET"
--- 3090.sh
+++ 3090.sh
@@ -4,7 +4,7 @@ GPUID=$1
 GPU_OFFSET=-400
 MEM_OFFSET=2000
 
-nvidia-smi -i $GPUID -pl 320
+nvidia-smi -i $GPUID -pl 322
 DISPLAY=:0 nvidia-settings --verbose \
     -a "[gpu:$GPUID]/GPUPowerMizerMode=1" \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffset[4]=$GPU_OFFSET" \
