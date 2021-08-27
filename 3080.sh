27963c108218e9a519dbfdc872c5d87ce76c5204
yao <yao.ntno@gmail.com>
2021-08-27 12:53:57 +0000

    reduce watt for circuit breaker

--- 3080.sh
+++ 3080.sh
@@ -4,7 +4,7 @@ GPUID=$1
 GPU_OFFSET=-300
 MEM_OFFSET=1930
 
-nvidia-smi -i $GPUID -pl 222
+nvidia-smi -i $GPUID -pl 220
 DISPLAY=:0 nvidia-settings --verbose \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffsetAllPerformanceLevels=$GPU_OFFSET" \
     -a "[gpu:$GPUID]/GPUMemoryTransferRateOffsetAllPerformanceLevels=$MEM_OFFSET"
