a333dc462316e74c42a24adb44021e489f1b4bab
yao <yao.ntno@gmail.com>
2021-09-12 01:15:52 +0000

    add persistance

--- 3090.sh
+++ 3090.sh
@@ -4,6 +4,7 @@ GPUID=$1
 GPU_OFFSET=-400
 MEM_OFFSET=2000
 
+nvidia-smi -i $GPUID -pm ENABLED
 nvidia-smi -i $GPUID -pl 320
 DISPLAY=:0 nvidia-settings --verbose \
     -a "[gpu:$GPUID]/GPUPowerMizerMode=1" \
