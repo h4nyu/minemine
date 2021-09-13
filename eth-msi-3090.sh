2e8dff614dc8b643b11ef6d9f4c94a0400cd5f17
xinyuan.yao <yao.ntno@gmail.com>
2021-09-13 12:01:45 +0000

    add

---
+++ eth-msi-3090.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+GPUID=$1
+GPU_OFFSET=-200
+MEM_OFFSET=2000
+
+nvidia-smi -i $GPUID -pm ENABLED
+nvidia-smi -i $GPUID -pl 285
+DISPLAY=:0 nvidia-settings --verbose \
+    -a "[gpu:$GPUID]/GPUPowerMizerMode=1" \
+    -a "[gpu:$GPUID]/GPUGraphicsClockOffset[4]=$GPU_OFFSET" \
+    -a "[gpu:$GPUID]/GPUMemoryTransferRateOffset[4]=$MEM_OFFSET"
