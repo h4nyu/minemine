4de53f2d9a3761e7640e548930626aea0da50e08
xinyuan.yao <yao.ntno@gmail.com>
2021-09-21 09:21:49 +0000

    gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080gigabyte 3080

--- eth-gigabyte-3080.sh
+++ eth-gigabyte-3080.sh
@@ -5,7 +5,7 @@ GPU_OFFSET=-200
 MEM_OFFSET=2100
 
 nvidia-smi -i $GPUID -pm ENABLED
-nvidia-smi -i $GPUID -pl 224
+nvidia-smi -i $GPUID -pl 225
 
 DISPLAY=:0 nvidia-settings --verbose \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffsetAllPerformanceLevels=$GPU_OFFSET" \
