5ff499d3936ec236465a35f5bd7942199a4435ee
yao <yao.ntno@gmail.com>
2021-10-28 07:36:08 +0000

    add A4000

---
+++ eth-elsa-A4000.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+GPUID=$1
+GPU_OFFSET=-200
+MEM_OFFSET=2100
+
+nvidia-smi -i $GPUID -pm ENABLED
+nvidia-smi -i $GPUID -pl 125
+
+DISPLAY=:0 nvidia-settings --verbose \
+    -a "[gpu:$GPUID]/GPUGraphicsClockOffsetAllPerformanceLevels=$GPU_OFFSET" \
+    -a "[gpu:$GPUID]/GPUMemoryTransferRateOffsetAllPerformanceLevels=$MEM_OFFSET"
