d682e01071729baf240e9eb80ff65b5cb61a1c44
yao <yao.ntno@gmail.com>
2022-02-10 12:17:27 +0000

    fun speed

--- config_xorg.sh
+++ config_xorg.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nvidia-xconfig -a --allow-empty-initial-configuration --cool-bits=31 
+nvidia-xconfig -a --allow-empty-initial-configuration --cool-bits=28 
--- eth-zotac-3080.sh
+++ eth-zotac-3080.sh
@@ -3,10 +3,13 @@
 GPUID=$1
 GPU_OFFSET=-200
 MEM_OFFSET=1850
+FAN_SPEED=80
 
 nvidia-smi -i $GPUID -pm ENABLED
 nvidia-smi -i $GPUID -pl 220
 
 DISPLAY=:0 nvidia-settings --verbose \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffsetAllPerformanceLevels=$GPU_OFFSET" \
-    -a "[gpu:$GPUID]/GPUMemoryTransferRateOffsetAllPerformanceLevels=$MEM_OFFSET"
+    -a "[gpu:$GPUID]/GPUMemoryTransferRateOffsetAllPerformanceLevels=$MEM_OFFSET" \
+    -a "[gpu:$GPUID]/GPUFanControlState=1" \
+    -a "[fan:$GPUID]/GPUTargetFanSpeed=$FAN_SPEED"
