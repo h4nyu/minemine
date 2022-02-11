79446b6d846363411e62e09e1f45c65c8afcff3c
h4nyu <yao.ntno@gmail.com>
2022-02-11 17:58:29 +0900

    mizer

--- eth-gigabyte-3090.sh
+++ eth-gigabyte-3090.sh
@@ -2,11 +2,10 @@
 
 GPUID=$1
 GPU_OFFSET=-200
-MEM_OFFSET=2000
+MEM_OFFSET=1950
 
 nvidia-smi -i $GPUID -pm ENABLED
-nvidia-smi -i $GPUID -pl 310
+nvidia-smi -i $GPUID -pl 280
 DISPLAY=:0 nvidia-settings --verbose \
-    -a "[gpu:$GPUID]/GPUPowerMizerMode=1" \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffset[4]=$GPU_OFFSET" \
     -a "[gpu:$GPUID]/GPUMemoryTransferRateOffset[4]=$MEM_OFFSET"
