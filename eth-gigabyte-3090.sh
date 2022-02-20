1534dbd075c95468c3025fc4d32df6bc1434f0f8
h4nyu <yao.ntno@gmail.com>
2022-02-20 15:59:07 +0900

    fix watt

--- eth-gigabyte-3090.sh
+++ eth-gigabyte-3090.sh
@@ -2,10 +2,10 @@
 
 GPUID=$1
 GPU_OFFSET=-200
-MEM_OFFSET=1950
+MEM_OFFSET=2000
 
 nvidia-smi -i $GPUID -pm ENABLED
-nvidia-smi -i $GPUID -pl 280
+nvidia-smi -i $GPUID -pl 290
 DISPLAY=:0 nvidia-settings --verbose \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffset[4]=$GPU_OFFSET" \
     -a "[gpu:$GPUID]/GPUMemoryTransferRateOffset[4]=$MEM_OFFSET"
