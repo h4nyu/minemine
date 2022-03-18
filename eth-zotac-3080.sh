341b3ad6b1a296fbfcb42900882d6b4be43a678c
yao <yao.ntno@gmail.com>
2022-03-18 12:34:14 +0000

    fix

--- eth-zotac-3080.sh
+++ eth-zotac-3080.sh
@@ -5,7 +5,7 @@ GPU_OFFSET=-200
 MEM_OFFSET=1750
 
 nvidia-smi -i $GPUID -pm ENABLED
-nvidia-smi -i $GPUID -pl 220
+nvidia-smi -i $GPUID -pl 215
 
 DISPLAY=:0 nvidia-settings --verbose \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffsetAllPerformanceLevels=$GPU_OFFSET" \
