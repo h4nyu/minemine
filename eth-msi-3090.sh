663cc914235adec4993593457cce3bce78f526f0
yao <yao.ntno@gmail.com>
2022-03-28 12:40:34 +0000

    reduce watt

--- eth-msi-3090.sh
+++ eth-msi-3090.sh
@@ -5,7 +5,7 @@ GPU_OFFSET=-200
 MEM_OFFSET=2150
 
 nvidia-smi -i $GPUID -pm ENABLED
-nvidia-smi -i $GPUID -pl 290
+nvidia-smi -i $GPUID -pl 280
 DISPLAY=:0 nvidia-settings --verbose \
     -a "[gpu:$GPUID]/GPUPowerMizerMode=1" \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffset[4]=$GPU_OFFSET" \
