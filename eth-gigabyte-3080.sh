279a2126e81e8d0acad5fec9a40e49160dfb461e
h4nyu <yao.ntno@gmail.com>
2022-04-02 23:37:41 +0900

    update

--- eth-gigabyte-3080.sh
+++ eth-gigabyte-3080.sh
@@ -2,10 +2,10 @@
 
 GPUID=$1
 GPU_OFFSET=-300
-MEM_OFFSET=1400
+MEM_OFFSET=1300
 
 nvidia-smi -i $GPUID -pm ENABLED
-nvidia-smi -i $GPUID -pl 230
+nvidia-smi -i $GPUID -pl 215
 
 DISPLAY=:0 nvidia-settings --verbose \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffsetAllPerformanceLevels=$GPU_OFFSET" \
