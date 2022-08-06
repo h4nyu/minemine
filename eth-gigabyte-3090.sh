933f39f4d2d7607d22d9bed1dc5093295cd3ccd2
yao <yao.ntno@gmail.com>
2022-08-06 13:32:07 +0000

    setup

--- eth-gigabyte-3090.sh
+++ eth-gigabyte-3090.sh
@@ -2,10 +2,10 @@
 
 GPUID=$1
 GPU_OFFSET=-200
-MEM_OFFSET=1350
+MEM_OFFSET=1000
 
 nvidia-smi -i $GPUID -pm ENABLED
-nvidia-smi -i $GPUID -pl 220
+nvidia-smi -i $GPUID -pl 230
 DISPLAY=:0 nvidia-settings --verbose \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffset[4]=$GPU_OFFSET" \
     -a "[gpu:$GPUID]/GPUMemoryTransferRateOffset[4]=$MEM_OFFSET"
