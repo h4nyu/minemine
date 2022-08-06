9a3e1af2f543b390ee3d1a716705bb2f7d8e5c77
yao <yao.ntno@gmail.com>
2022-08-06 13:34:28 +0000

    stable

--- eth-kurouto-3080.sh
+++ eth-kurouto-3080.sh
@@ -2,10 +2,10 @@
 
 GPUID=$1
 GPU_OFFSET=-200
-MEM_OFFSET=1100
+MEM_OFFSET=1000
 
 nvidia-smi -i $GPUID -pm ENABLED
-nvidia-smi -i $GPUID -pl 205
+nvidia-smi -i $GPUID -pl 200
 
 DISPLAY=:0 nvidia-settings --verbose \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffsetAllPerformanceLevels=$GPU_OFFSET" \
