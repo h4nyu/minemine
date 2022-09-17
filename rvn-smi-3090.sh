ac50baa1a1f413a152a14be8e4c3a89df75aa2f6
yao <yao.ntno@gmail.com>
2022-09-17 15:08:12 +0000

    add etc

--- rvn-smi-3090.sh
+++ rvn-smi-3090.sh
@@ -2,10 +2,11 @@
 
 GPUID=$1
 GPU_OFFSET=-200
-MEM_OFFSET=1000
+MEM_OFFSET=2000
 
 nvidia-smi -i $GPUID -pm ENABLED
-nvidia-smi -i $GPUID -pl 270
+nvidia-smi -i $GPUID -pl 320
 DISPLAY=:0 nvidia-settings --verbose \
+    -a "[gpu:$GPUID]/GPUPowerMizerMode=1" \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffset[4]=$GPU_OFFSET" \
     -a "[gpu:$GPUID]/GPUMemoryTransferRateOffset[4]=$MEM_OFFSET"
