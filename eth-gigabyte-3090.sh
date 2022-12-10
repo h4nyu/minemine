3445b9486617f3ea53316fddf6926ff7e69e240f
yao <yao.ntno@gmail.com>
2022-12-10 18:55:05 +0000

    add

--- eth-gigabyte-3090.sh
+++ eth-gigabyte-3090.sh
@@ -5,7 +5,7 @@ GPU_OFFSET=-200
 MEM_OFFSET=1000
 
 nvidia-smi -i $GPUID -pm ENABLED
-nvidia-smi -i $GPUID -pl 270
+    nvidia-smi -i $GPUID -pl 260
 DISPLAY=:0 nvidia-settings --verbose \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffset[4]=$GPU_OFFSET" \
     -a "[gpu:$GPUID]/GPUMemoryTransferRateOffset[4]=$MEM_OFFSET"
--- eth-msi-3090.sh
+++ eth-msi-3090.sh
@@ -2,10 +2,10 @@
 
 GPUID=$1
 GPU_OFFSET=-200
-MEM_OFFSET=2000
+MEM_OFFSET=1000
 
 nvidia-smi -i $GPUID -pm ENABLED
-nvidia-smi -i $GPUID -pl 270
+nvidia-smi -i $GPUID -pl 260
 DISPLAY=:0 nvidia-settings --verbose \
     -a "[gpu:$GPUID]/GPUPowerMizerMode=1" \
     -a "[gpu:$GPUID]/GPUGraphicsClockOffset[4]=$GPU_OFFSET" \
